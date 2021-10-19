a05cac61e56465d538d162c67f33000a4c4f62a3
koresachin7 <koresachin7@gmail.com>
2021-10-19 22:16:03 +0530

    Added :- Welcome message

---
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeePayRollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Pay Roll Service");
+    }
+}

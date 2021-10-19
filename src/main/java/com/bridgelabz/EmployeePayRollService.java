24c99d9cce95b86d33e5863b3d8b14dfc2502fd2
koresachin7 <koresachin7@gmail.com>
2021-10-19 22:26:05 +0530

    Added:- To create Employee PayRoll Service and to read and write

--- src/main/java/com/bridgelabz/EmployeePayRollService.java
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -1,7 +1,40 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Purpose - To implement Employee PayRoll Service System
+ * @author - Sachin Kore
+ * @version - 16.0
+ */
 public class EmployeePayRollService {
+    public List<EmployeePayRoll> employeePayrollList = new ArrayList<>();
+
+    public EmployeePayRollService(ArrayList<EmployeePayRoll> employeePayrollList) {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Pay Roll Service");
+        System.out.println("WelCome to Employee Payroll Service");
+        ArrayList<EmployeePayRoll> employeePayrollList = new ArrayList<EmployeePayRoll>();
+        EmployeePayRollService employeePayrollService = new EmployeePayRollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeeData(consoleInputReader);
+        employeePayrollService.writeEmployeeData();
+    }
+
+    private void writeEmployeeData() {
+        System.out.println("\nwriting employee payroll data to console\n"+ employeePayrollList);
+    }
+
+    private void readEmployeeData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee id: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee name ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee salary ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayRoll(id,name,salary));
     }
 }
